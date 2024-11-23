b090c7d1cd13f0e8e9c5d8b15267dab440ada14b
alfandi rifaul <125331290+alfandirifaul@users.noreply.github.com>
2024-11-23 21:36:32 +0700

    upload

---
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AttendeeResource;
+use Illuminate\Http\Request;
+use App\Models\Event;
+use App\Models\Attendee;
+use App\Http\Traits\CanLoadRelationships;
+
+class AttendeeController extends Controller
+{
+    use CanLoadRelationships;
+
+    private array $relations = ['user'];
+
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')
+            ->except(['index', 'show', 'update']);
+        $this->middleware('throttle:api')
+            ->only(['store', 'index']);
+        $this->authorizeResource(Attendee::class, 'attendee');
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Event $event)
+    {
+        $attendees = $this->loadRelationships(
+            $event->attendees()->latest()
+        );
+
+        return AttendeeResource::collection(
+            $attendees->paginate()
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Event $event)
+    {
+        $attendee = $this->loadRelationships(
+            $event->attendees()->create([
+                'user_id' => $request->user()->id
+            ])
+        );
+
+        return new AttendeeResource($attendee);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Event $event, Attendee $attendee)
+    {
+
+        return new AttendeeResource(
+            $this->loadRelationships($attendee)
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Event $event, Attendee $attendee)
+    {
+//        $this->authorize('delete-attendee', [$event, $attendee]);
+        $attendee->delete();
+
+        return response(status: 204);
+    }
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Attendee;
+use App\Models\Event;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    protected $policies = [
+
+    ];
+
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+//        Gate::define('update-event', function ($user, Event $event){
+//            return $user->id === $event->user_id;
+//        });
+//
+//        Gate::define('delete-attendee', function($user, Event $event, Attendee $attendee){
+//            return $user->id === $event->user_id ||
+//                $user->id === $attendee->user_id;
+//        });
+    }
+}
